5640b893f707f595cf2d20f386e63b6c45513c6f
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-04-24 10:40:08 +0530

    [COMPLETED] load_validate file

--- App/apps/ingestion/load_validate.py
+++ App/apps/ingestion/load_validate.py
@@ -115,3 +115,194 @@ class LoadValidate:
         
         except Exception as e:
             self.logger.exception('Exception raised while Replacing Missing Values with NULL: %s' % e)
+    
+
+    def archive_old_files(self):
+
+        now = datetime.now()
+        date = now.date()
+        time = now.strftime("%H%M%S")
+
+        try:
+
+            
+            self.logger.info("Start of archiving old rejected files...")
+            source = self.data_path + '_rejects/'
+
+            if os.path.isdir(source):
+                path = self.data_path + '_archive'
+                if not os.path.isdir(path):
+                    os.makedirs(path)
+                
+                dest = path + '/reject_' + str(date) + '_' + str(time)
+                files = os.listdir(source)
+
+                for f in files:
+
+                    if not os.path.isdir(dest):
+                        os.makedirs(dest)
+                    if f not in os.listdir(dest):
+                        shutil.move(source + f, dest)
+            
+            self.logger.info('End of Archiving Old Rejected Files...')
+
+            
+            self.logger.info("Start of archiving old Validated files...")
+            source = self.data_path + '_validation/'
+
+            if os.path.isdir(source):
+                path = self.data_path + '_archive'
+                if not os.path.isdir(path):
+                    os.makedirs(path)
+                
+                dest = path + '/validation_' + str(date) + '_' + str(time)
+                files = os.listdir(source)
+
+                for f in files:
+
+                    if not os.path.isdir(dest):
+                        os.makedirs(dest)
+                    if f not in os.listdir(dest):
+                        shutil.move(source + f, dest)
+            
+            self.logger.info('End of Archiving Old Validated Files...')
+
+            
+            self.logger.info('Start of Archiving Old Processed Files...')
+            source = self.data_path + '_processed/'
+
+            if os.path.isdir(source):
+                path = self.data_path + '_archive'
+                if not os.path.isdir(path):
+                    os.makedirs(path)
+                    
+                dest = path + '/processed_' + str(date) + '_' + str(time)
+                files = os.listdir(source)
+
+                for f in files:
+
+                    if not os.path.isdir(dest):
+                        os.makedirs(dest)
+                    if f not in os.listdir(dest):
+                        shutil.move(source + f, dest)
+                
+            self.logger.info('End of Archiving Old Processed Files...')
+
+            
+            self.logger.info('Start of Archiving Old Result Files...')
+            source = self.data_path + '_results/'
+
+            if os.path.isdir(source):
+                path = self.data_path + '_archive'
+                if not os.path.isdir(path):
+                    os.makedirs(path)
+                    
+                dest = path + '/results_' + str(date) + '_' + str(time)
+                files = os.listdir(source)
+
+                for f in files:
+
+                    if not os.path.isdir(dest):
+                        os.makedirs(dest)
+                    if f not in os.listdir(dest):
+                        shutil.move(source + f, dest)
+            self.logger.info('End of Archiving Old Result Files...')
+        
+        except Exception as e:
+            self.logger.exception('Exception raised while Archiving Old Rejected Files: %s' % e)
+            raise e
+
+    def move_processed_files(self):
+
+        try:
+
+            self.logger.info('Start of Moving Processed Files...')
+            for file in listdir(self.data_path):
+                shutil.move(self.data_path + '/' + file, self.data_path + '_processed')
+                self.logger.info("Moved the already processed file %s" % file)
+
+            self.logger.info('End of Moving processed Files...')
+
+        except Exception as e:
+
+            self.logger.exception('Exception raised while moving processed files: %s' %e)
+            raise e
+
+
+    def validate_trainset(self):
+
+        try:
+
+            self.logger.info('Start of Data Load, validation and transformation')
+
+            # archive old files
+            self.archive_old_files()
+
+            # extracting values from training schema
+            column_names, number_of_columns = self.values_from_schema('schema_train')
+
+            # validate column length in the file
+            self.validate_column_length(number_of_columns)
+
+            # validate if any column has all values missing
+            self.validate_missing_values()
+
+            # replacing blanks in the csv file with "Null" values
+            self.replace_missing_values()
+            
+            # create database with given name, if present open the connection! Create table with columns given in schema
+            self.dbOperation.create_table('training','training_raw_data_t',column_names)
+            
+            # insert csv files in the table
+            self.dbOperation.insert_data('training','training_raw_data_t')
+            
+            # export data in table to csv file
+            self.dbOperation.export_csv('training','training_raw_data_t')
+            
+            # move processed files
+            self.move_processed_files()
+            
+            self.logger.info('End of Data Load, validation and transformation')
+        
+        except Exception:
+            self.logger.exception('Unsuccessful End of Data Load, validation and transformation')
+            raise Exception
+
+    def validate_predictset(self):
+
+        try:
+            
+            self.logger.info('Start of Data Load, validation and transformation')
+            
+            # archive old rejected files
+            self.archive_old_files()
+           
+            # extracting values from schema
+            column_names, number_of_columns = self.values_from_schema('schema_predict')
+            
+            # validating column length in the file
+            self.validate_column_length(number_of_columns)
+            
+            # validating if any column has all values missing
+            self.validate_missing_values()
+            
+            # replacing blanks in the csv file with "Null" values
+            self.replace_missing_values()
+            
+            # create database with given name, if present open the connection! Create table with columns given in schema
+            self.dbOperation.create_table('prediction','prediction_raw_data_t', column_names)
+            
+            # insert csv files in the table
+            self.dbOperation.insert_data('prediction','prediction_raw_data_t')
+            
+            # export data in table to csv file
+            self.dbOperation.export_csv('prediction','prediction_raw_data_t')
+            
+            # move processed files
+            self.move_processed_files()
+            
+            self.logger.info('End of Data Load, validation and transformation')
+        
+        except Exception:
+            self.logger.exception('Unsuccessful End of Data Load, validation and transformation')
+            raise Exception

7851165133275c2ba6198a8f30beed3a388e82e8
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-04-25 17:24:45 +0530

    [ADDED] impute value function

--- App/apps/preprocess/preprocessor.py
+++ App/apps/preprocess/preprocessor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
 from apps.core.logger import Logger
+import json
 
 
 class Preprocessor:
@@ -70,3 +71,81 @@ class Preprocessor:
         except Exception as e:
             self.logger.exception('Exception raised while finding missing values:'+str(e))
             raise Exception()
+    
+
+    def impute_missing_values(self, data):
+
+        self.data = data
+        
+        try:
+
+            self.logger.info("Start of imputing missing values...")
+            imputer = KNNImputer(n_neighbors=3, weights='uniform', missing_values=np.nan)
+            self.new_array = imputer.fit_transform(self.data) # impute the missing values
+            # convert the nd-array returned in the step above to a Data frame
+            self.new_data = pd.DataFrame(data=self.new_array, columns=self.data.columns)
+            self.logger.info("End of imputing missing values...")
+            return self.new_data
+        
+        except Exception as e:
+            self.logger.exception('Exception raised while imputing missing values:'+str(e))
+            raise Exception()
+    
+    
+    def feature_encoding(self, data):
+
+        try:
+
+            self.logger.info("Start of feature encoding...")
+            self.new_data = data.select_dtypes(include=['object']).copy()
+            # using the dummy encoding to encode the categorical columns to numerical ones
+            for col in self.new_data.columns:
+                self.new_data = pd.get_dummies(self.new_data, columns=[col], prefix=[col], drop_first=True)
+
+            self.logger.info('End of feature encoding...')
+            return self.new_data
+
+        except Exception as e:
+            self.logger.exception('Exception raised while feature encoding:' + str(e))
+            raise Exception()
+    
+    
+    def split_features_label(self, data, label_name):
+
+        self.data = data
+
+        try:
+
+            self.logger.info('Start of splitting features and label ...')
+            self.X=self.data.drop(labels=label_name,axis=1) # drop the columns specified and separate the feature columns
+            self.y=self.data[label_name] # Filter the Label columns
+            self.logger.info('End of splitting features and label ...')
+            return self.X,self.y
+
+        except Exception as e:
+            self.logger.exception('Exception raised while splitting features and label:' + str(e))
+            raise Exception()
+
+    def final_predictset(self,data):
+
+        try:
+
+            self.logger.info('Start of building final predictset...')
+            with open('apps/database/columns.json', 'r') as f:
+                data_columns = json.load(f)['data_columns']
+                f.close()
+            df = pd.DataFrame(data=None, columns=data_columns)
+            df_new = pd.concat([df, data], ignore_index=True,sort=False)
+            data_new = df_new.fillna(0)
+            self.logger.info('End of building final predictset...')
+            return data_new
+
+        except ValueError:
+            self.logger.exception('ValueError raised while building final predictset')
+            raise ValueError
+        except KeyError:
+            self.logger.exception('KeyError raised while building final predictset')
+            raise KeyError
+        except Exception as e:
+            self.logger.exception('Exception raised while building final predictset: %s' % e)
+            raise e
